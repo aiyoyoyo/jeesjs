5cc05b24b5ee5a5aa2ce1b99a942d0c58ab7803c
unknown <aiyoyoyo@163.com>
2017-08-23 13:19:10 +0800

    Update test ui

--- demo/UITest.js
+++ demo/UITest.js
@@ -1,21 +1,46 @@
 function UITest(){
 	var Mod_Test = new jeesjs.Module();
 	Mod_Test.enter = function() {
-		var d0 = new createjs.Bitmap( jeesjs.QM.getSource( "jpg" ) );
-		jeesjs.APP._contar.addChild( d0 );
+		function test( _e ){
+			console.log( _e.target.id );
+		}
 		
-		var d1 = new createjs.Bitmap( jeesjs.QM.getSource( "png" ) );
-		jeesjs.APP._contar.addChild( d1 );
-		d1.x = 200;
-		d1.alpha = 0.5
-		d1.visible = true;
+//		var w = new jeesjs.Widget();
+//		w.setSize( 100, 100 );
 		
-		d0.addEventListener( "click", function(){
-			console.log("click d0");
-		});
-		d1.addEventListener( "click", function(){
-			console.log("click d1");
-		});
+		var p = new jeesjs.Panel();
+		p.setSize( 200 ,200 );
+		p.onClick( test );
+		
+//		w.addChild( p );
+		
+		var p2 = new jeesjs.Panel();
+		p2.setColor( "#ffff00" );
+		p2.setPosition( 50, 50 );
+		p2.setSize( 200 ,200 );
+		p2.onClick( test );
+		
+		p.addChild( p2 );
+//		var w2 = new jeesjs.Widget();
+//		w2.setSize( 200 ,200 );
+//		w2.addChild( p2 );
+//		w.addChild( p2 );
+		
+//		jeesjs.CM.addWidget( p );
+		jeesjs.CM.addWidget( p );
+		
+//		jeesjs.CM.addWidget( p, w );
+//		var d0 = new createjs.Bitmap( jeesjs.QM.getSource( "jpg" ) );
+//		jeesjs.APP._contar.addChild( d0 );
+		
+//		var d1 = new createjs.Bitmap( jeesjs.QM.getSource( "png" ) );
+//		jeesjs.APP._contar.addChild( d1 );
+//		d1.x = 200;
+//		d1.alpha = 0.5
+//		d1.visible = true;
+//		
+//		d0.addChild( d1 );
+//		jeesjs.CM.addWidget( d0 );
 	};
 	Mod_Test.leave = function() { console.log("--Mod_Test leave"); };
 	Mod_Test.update = function( _t ) { console.log("--Mod_Test update" ); }
